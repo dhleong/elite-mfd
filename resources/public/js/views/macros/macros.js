9e9a34476237511de85fe2cb6ea78fd90ed42c13
dhleong <falcone88@gmail.com>
2015-01-22 13:29:29 -0500

    Add UI for creating and editing macros
    Refs #6

--- resources/public/js/views/macros/macros.js
+++ resources/public/js/views/macros/macros.js
@@ -1,5 +1,5 @@
 'use strict';
-/* global angular */
+/* global angular, _ */
 /* jshint indent: false */
 
 angular.module('emfd.views.macros', ['ngRoute'])
@@ -16,18 +16,57 @@ angular.module('emfd.views.macros', ['ngRoute'])
         function($scope, cmdr) {
     var bindings = cmdr.prop('bindings', {});
     var macros = cmdr.prop('macros', []);
+
     $scope.bindings = bindings();
-    $scope.macros = macros();
-    $scope.newMacro = {};
-    console.log($scope.macros);
+    $scope.macros = _.toArray(macros()); // copy
+
+    // add a blank row for creating new macros
+    var addMacroPlaceholder = function() {
+        $scope.macros.push({name:"", value:[], isNew: true});
+    }
+    addMacroPlaceholder();
+
+    /*
+     * methods
+     */
+
+    $scope.addBinding = function(macro) {
+        macro.value.push(Object.keys($scope.bindings)[0]);
+    }
+
+    $scope.checkMacroItem = function(macro, itemIndex) {
+        if (!macro.value[itemIndex])
+            macro.value.splice(itemIndex, 1);
+
+        if (!macro.isNew) {
+            // real macro; update it
+            // NB: ensure we don't include the placeholder
+            macros($scope.macros.slice(0, -1));
+        }
+    }
 
     $scope.saveBindings = function() {
         bindings($scope.bindings);
     }
 
-    $scope.saveMacro = function() {
+    $scope.createMacro = function(macro) {
+        // mark non-new
+        delete macro.isNew;
+
+        // save macros
         console.log($scope.macros);
-        console.log("NEW", $scope.newMacro);
+        macros($scope.macros);
+
+        // add new placeholder
+        addMacroPlaceholder();
+    }
+
+    $scope.deleteMacro = function(macrosIndex) {
+        // kill it
+        $scope.macros.splice(macrosIndex, 1);
+
+        // NB: ensure we don't include the placeholder
+        macros($scope.macros.slice(0, -1));
     }
 }])
 
